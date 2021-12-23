8e10591af84adba03707e69dc8e898ef1a886922
holand76 <thegreath76@gmail.com>
2021-12-23 05:37:20 -0300

    rand()

--- programa.c
+++ programa.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int meuabs(int valor){
 //funcao para converter em positivo sempre
@@ -17,13 +19,17 @@ int main(){
 	printf("* Bem vindo ao nosso jogo de adivinhacao *\n");	
 	printf("******************************************\n");
 
-	int numerosecreto = 42;
+	int seed = time(0);
+	srand(seed);
+
+	int numerosecreto = rand() % 100;
 	int chute;
 	int tentativas = 1;
-	int pontos = 2000;
+	double pontos = 2000;
 
 	while(1) {
-		printf("voce tem %d pontos\n\n",pontos);
+		printf("debug o numero eh %d ",numerosecreto);
+		printf("voce tem %.2lf pontos\n\n",pontos);
 		printf("Tentativa %d\n\n", tentativas);
 		printf("Qual e o seu chute? ");
 		scanf("%d", &chute);
@@ -38,7 +44,7 @@ int main(){
 		int acertou = (chute == numerosecreto);
 		int maior = chute > numerosecreto;
 
-		if(acertou) {
+ 		if(acertou) {
 			printf("Parabens! Voce acertou!\n\n");
 			printf("Jogue de novo, voce e um bom jogador!!\n\n");
 
@@ -56,12 +62,12 @@ int main(){
 			//se ele acertar essa parte do codigo nao eh executada entao para economizar linha
 		//colocar o retirar de pontos aquie ao envez de no else if e no else
 		
-		pontos -= ((meuabs(chute - numerosecreto))/2);
+		pontos -= ((meuabs(chute - numerosecreto))/2.0);
 
 		tentativas++;
 	}
 
 	printf("Fim de jogo.\n\n");
 	printf("Voce acertou em %d tentativas\n\n", tentativas);
-	printf("E terminou com %d pontos \n\n",pontos);
+	printf("E terminou com %.2lf pontos \n\n",pontos);
 }
